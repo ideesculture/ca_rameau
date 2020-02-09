017dff35628aa92752c12f78c09d2a7785d49e05
GautierMichelin <gautiermichelin@users.noreply.github.com>
2020-02-09 18:37:39 +0100

    Update Rameau.php

--- Rameau.php
+++ Rameau.php
@@ -39,8 +39,8 @@
  *
  */
 
-require_once __CA_LIB_DIR__ . "/core/Plugins/IWLPlugInformationService.php";
-require_once __CA_LIB_DIR__ . "/core/Plugins/InformationService/BaseInformationServicePlugin.php";
+require_once __CA_LIB_DIR__ . "/Plugins/IWLPlugInformationService.php";
+require_once __CA_LIB_DIR__ . "/Plugins/InformationService/BaseInformationServicePlugin.php";
 
 global $g_information_service_settings_Rameau;
 $g_information_service_settings_Rameau = array(
